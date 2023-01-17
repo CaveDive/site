15257b6ac40d62595ce1bf1cd627bccb189fc86f
maxduh <maxduhmine@gmail.com>
2023-01-17 19:56:40 +0200

    update2

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ document.addEventListener("mousemove", parallax);
       function parallax(event) {
         this.querySelectorAll(".mouse").forEach((shift) => {
           const position = shift.getAttribute("value");
-          const x = (window.innerWidth - event.pageX * position) / 90+window.innerWidth*0.05/2;
-          const y = (window.innerHeight - event.pageY * position) / 90+window.innerHeight*0.05/2;
+          const x = (window.innerWidth - event.pageX * position) / 90+window.innerWidth*0.1/2;
+          const y = (window.innerHeight - event.pageY * position) / 90+window.innerHeight*0.10/2;
   
           shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
         });
