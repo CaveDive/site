98431cc4d7a48dcb40e878779aeb070bd3ace5b5
maxduh <maxduhmine@gmail.com>
2023-01-18 12:32:21 +0200

    .

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ document.getElementById("bg").style.backgroundImage = "linear-gradient(#00000000
 
 window.addEventListener('deviceorientation',function(e){
   const x = Math.round(e.beta);
-  const y = Math.round(e.gamma);
+  const y = -Math.round(e.gamma);
   const z = Math.round(e.alpha);
 
   if(j){
