c862cb5b6c50585d429b2cd339c6b7c1442e5a40
maxduh <maxduhmine@gmail.com>
2023-01-19 15:13:47 +0200

    .

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function parallax(event) {
     const x = (window.innerWidth - event.pageX * position) / 90+window.innerWidth*0.1/2;
     const y = (window.innerHeight - event.pageY * position) / 90+window.innerHeight*0.10/2;
 
-    shift.style.transform = `translateX(${x}px) translateY(${y+40}px)`;
+    shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
 }
 
@@ -19,7 +19,7 @@ bgs.push("media/LushCave.webp", "media/IceCave.webp","media/LavaCave.webp")
 document.getElementById("bg").style.backgroundImage = "linear-gradient(#00000000,#000000FF),url("+bgs[getRandomInt(bgs.length)]+")";
 
 window.addEventListener('deviceorientation',function(e){
-  const x = Math.round(e.beta);
+  const x = Math.round(e.beta)+40;
   const y = -Math.round(e.gamma);
   const z = Math.round(e.alpha);
 
